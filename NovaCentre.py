7afa19f308e67e85a9d6813b22d74dc671b8f7b5
Alvina SV <vastinensis@gmail.com>
2024-01-31 13:08:26 -0800

    1st functional commit pt 2. Python code did not update

--- NovaCentre.py
+++ NovaCentre.py
@@ -1,4 +1,3 @@
-
 import xml.etree.ElementTree as ET
 
 class NovaCentre:
@@ -36,13 +35,60 @@ class NovaCentre:
 
         return world
 
-    # Placeholder methods for handling commands
     def process_input(self, user_input):
-        pass
+        commands = user_input.lower().split()
+        if not commands:
+            return "You need to enter a command."
+        
+        if commands[0] == 'look':
+            return self.handle_look()
+        elif commands[0] == 'go':
+            return self.handle_go(commands[1:])
+        elif commands[0] == 'take':
+            return self.handle_take(commands[1:])
+        else:
+            return "I don't understand that command."
+
+    def handle_look(self):
+        location = self.game_state['location']
+        location_data = self.game_world.get(location, {})
+        return location_data.get('description', 'There is nothing special here.')
+
+    def handle_go(self, direction):
+        if not direction:
+            return "Go where?"
+        direction = direction[0]
+        current_location = self.game_state['location']
+        new_location = self.game_world[current_location]['connections'].get(direction)
+        if new_location:
+            self.game_state['location'] = new_location
+            return f"You go {direction} to {new_location}."
+        else:
+            return "You can't go that way."
+
+    def handle_take(self, item):
+        if not item:
+            return "Take what?"
+        item = item[0]
+        current_location = self.game_state['location']
+        if item in self.game_world[current_location]['items']:
+            self.game_state['inventory'].append(item)
+            del self.game_world[current_location]['items'][item]
+            return f"You take the {item}."
+        else:
+            return f"There is no {item} here to take."
 
     def run(self):
-        pass
+        print("Welcome to the Text Adventure!")
+        while True:
+            user_input = input("> ")
+            if user_input.lower() == 'exit':
+                print("Exiting game.")
+                break
+            output = self.process_input(user_input)
+            print(output)
 
 # Example usage
-# game = NovaCentre()
-# game.load_world('your_game_world.xml')
+game = NovaCentre()
+game.load_world('game_world.xml')
+game.run()
